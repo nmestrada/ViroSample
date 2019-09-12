18be4c5770ef6119a27021849c96d478888a0c83
Natalie Estrada <nmestrad@gmail.com>
2019-09-11 22:44:08 -0500

    added coffee

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Alert} from 'react-native';
 
 import {
   ViroARScene,
@@ -18,6 +18,7 @@ import {
   ViroAnimations
 } from 'react-viro';
 
+
 export default class HelloWorldSceneAR extends Component {
 
   constructor() {
@@ -25,7 +26,9 @@ export default class HelloWorldSceneAR extends Component {
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+      text : "Initializing AR...",
+      object: {displayObject:true, objectSource:require('./res/coffee_mug/object_coffee_mug.vrx'), x:0,y:0,z:0},
+      rotation: [0,0,0]
     };
 
     // bind 'this' to functions
@@ -33,44 +36,63 @@ export default class HelloWorldSceneAR extends Component {
   }
 
   render() {
+    const object = this.state.object;
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
         <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-        <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} animation={{name: "rotate", run: true, loop: true}}/>
         <ViroAmbientLight color={"#aaaaaa"} />
-        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]}
-          position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
-        <ViroARPlaneSelector>
-            <ViroNode position={[0,-1,0]} dragType="FixedToWorld" onDrag={()=>{}} >
-                <Viro3DObject
-                            source={require('./res/emoji_smile/emoji_smile.vrx')}
-                            resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
-                                require('./res/emoji_smile/emoji_smile_normal.png'),
-                                require('./res/emoji_smile/emoji_smile_specular.png')]}
-                            position={[0, .5, 0]}
-                            scale={[.2, .2, .2]}
-                            type="VRX" />
-            </ViroNode>
-        </ViroARPlaneSelector>
+        <ViroSpotLight
+          innerAngle={5}
+          outerAngle={20}
+          direction={[0,1,0]}
+          position={[0, 1, 0]}
+          color="#ffffff"
+          castsShadow={true}
+          shadowNearZ={.1}
+          shadowFarZ={6}
+          shadowOpacity={.9}
+          />
+        <ViroNode position={[0,-1,0]} dragType="FixedDistanceOrigin" onDrag={()=>{}} >
+            <Viro3DObject
+                        source={this.state.object.objectSource}
+                        position={[0, 0.5, -0.5]}
+                        scale={[.2, .2, .2]}
+                        onRotate={this._onRotate}
+                        ref={VR => (this._Viro3DObject = VR)}
+                        type="VRX" />
+        </ViroNode>
       </ViroARScene>
     );
+  }
+  _setSpotLightRef(component) {
+    this.spotLight = component;
+  }
+
+  _setARNodeRef(component) {
+    this.arNodeRef = component;
   }
 
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Hello World!"
+        text : "Welcome to Fullstack!"
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
   }
+
+  _onRotate = (rotateState, rotationFactor, source) => {
+    this._Viro3DObject.setNativeProps({
+      rotation: [0, 0 + rotationFactor, 0]
+    });
+  };
 }
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
     fontFamily: 'Arial',
-    fontSize: 30,
+    fontSize: 25,
     color: '#ffffff',
     textAlignVertical: 'center',
     textAlign: 'center',  
